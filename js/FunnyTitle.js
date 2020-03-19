aa65df65fff49d68ae9fc5520aa784290c4ca169
TUFZ <waringhu@163.com>
2020-03-20 01:32:57 +0800

    Site updated: 2020-03-20 01:32:56

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,9 +1,9 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>// 浏览器搞笑标题
+// 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/funny.ico");
+        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '再会 (っ °Д °;)っ';
         clearTimeout(titleTime);
     }
