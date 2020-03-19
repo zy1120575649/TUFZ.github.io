a8303eb457a2def5be93f47e334b0540776aadc0
TUFZ <waringhu@163.com>
2020-03-20 01:43:58 +0800

    Site updated: 2020-03-20 01:43:56

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,17 +1,19 @@
 // 浏览器搞笑标题
+// 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "/funny.ico");
         document.title = '再会 (っ °Д °;)っ';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '欢迎光临 O(∩_∩)O' + OriginTitle;
+        document.title = '(欢迎光临 O(∩_∩)O' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
     }
 });
+
